8baa5c0f3bdf357ad2236fb2d7afe2d4ec77e5a2
Kevin Lin <stuy.kevinlin@gmail.com>
2019-07-22 14:22:57 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,40 @@
+var good = document.getElementById('good')
+var ok = document.getElementById('ok')
+var bad = document.getElementById('bad')
+
+var goodtext = document.getElementById('goodtext');
+var oktext = document.getElementById('oktext');
+var badtext = document.getElementById('badtext');
+
+var enjoy = document.getElementById('enjoy');
+var goodbye = document.getElementById('goodbye');
+
+var textLst = [goodtext, oktext, badtext, enjoy, goodbye];
+
+var clicked = false;
+
+var showText = function(i) {
+    textLst[i].style.opacity = 1;
+    setTimeout(function() {
+        textLst[3].style.opacity = 1;
+        setTimeout(function() {
+            textLst[4].style.opacity = 1;
+            textLst[4].style.cursor = "pointer";
+        }, 1500);
+    }, 1000);
+}
+
+var showWrap = function(i) {
+    if (!clicked) {
+        clicked = true;
+        showText(i)
+    }
+}
+
+textLst[4].addEventListener('click', function(e) {
+    clicked = false;
+    textLst[4].style.cursor = "default";
+    for (var i = 0; i < 5; i++) {
+        textLst[i].style.opacity = 0;
+    }
+});
